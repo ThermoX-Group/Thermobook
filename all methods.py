30631887aa48e41ce0be84a59eb5332118347315
Thermopylae <amirmahdi.brzgr1386@gmail.com>
2024-10-11 20:26:05 +0330

    dasdwdsada

---
+++ all methods.py
@@ -0,0 +1,15 @@
+friends=['qomi','digari','hakim','galini','ardabili','ermia']
+langs=['js','py','c++','java']
+# friends.append('ghahreman')
+# friends.extend(langs)
+# friends.pop(0)
+# friends.remove('ermia')
+# friends.reverse()
+# friends.sort()
+langs.insert(2,'c')
+
+
+
+
+# print(friends)
+print(langs)
