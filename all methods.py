dcb434ef9c3986acc62de4bda38ca2ce3c0e366d
Thermopylae <amirmahdi.brzgr1386@gmail.com>
2024-10-11 20:28:32 +0330

    dwadas

--- all methods.py
+++ all methods.py
@@ -6,10 +6,11 @@ langs=['js','py','c++','java']
 # friends.remove('ermia')
 # friends.reverse()
 # friends.sort()
-langs.insert(2,'c')
+# langs.insert(2,'c')
+# copy_langs=langs.copy()
+# print(copy_langs)
 
 
 
-
-# print(friends)
-print(langs)
+print(friends)
+# print(langs)
---
+++ extend.py
@@ -0,0 +1,4 @@
+items1=['kir','kos','koon']
+items2=['an','goh','zena']
+items1.extend(items2)
+print(items1)
